b034a208bbfbb94fa18660609842020e2c10f577
ahmadsanny2 <ahmadsanijabarulloh.02@gmail.com>
2025-09-20 10:44:55 +0700

    Change layout, color pallete, and update section hero, about-me, skills, and project

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,7 @@
 function toggleMenu() {
     const menu = document.getElementById('menu');
     menu.classList.toggle('hidden');
+
+    const menuIcon = document.getElementsByClassName('fa-bars');
+    menuIcon[0].classList.toggle('fa-xmark');
 }
